7d14dee5d04a9e24cedc05b96f503a43ccbe2292
sunnamed434 <cetnikletsplay@gmail.com>
2025-08-16 17:17:09 +0300

    Updates

--- src/middleware.ts
+++ src/middleware.ts
@@ -78,7 +78,8 @@ export const config = {
          * - _next/static (static files)
          * - _next/image (image optimization files)
          * - favicon.ico (favicon file)
+         * - favicon.jpg (favicon file)
          */
-        '/((?!api|_next/static|_next/image|favicon.ico).*)',
+        '/((?!api|_next/static|_next/image|favicon.ico|favicon.jpg).*)',
     ],
 }; 
