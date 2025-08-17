436b1fdad3bb48b5ceedffa96e9e67fc43846b5d
sunnamed434 <cetnikletsplay@gmail.com>
2025-08-17 16:27:27 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,15 @@
 import type { Metadata } from 'next';
 import './globals.css';
+
 export const metadata: Metadata = {
     title: 'Safeturned',
     description: 'Scan plugins for a backdoor!',
 };
+
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
-        <html lang="en">
-            <body className="">{children}</body>
+        <html>
+            <body>{children}</body>
         </html>
     );
 }
